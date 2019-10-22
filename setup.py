632a5d7bf7fb4c3af4f9ca7f248dd351d7da45bd
Tiphaine Phe-Neau <tiph.pheneau@gmail.com>
2019-10-22 14:57:00 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An implementation of mobility traces anonymization technique as defined in "Time Distortion Anonymization for the Publication of Mobility Data with High Utility" by Primault et al.',
+    author='T. Phe-Neau',
+    license='MIT',
+)
